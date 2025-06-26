a15e97810b28277a7dada4dc715fb0e984e46679
TradingIntent <laneboyth@gmail.com>
2025-06-26 07:08:31 +0100

    Switch bonkscreener Netlify function to native fetch

--- netlify/functions/bonkscreener.js
+++ netlify/functions/bonkscreener.js
@@ -1,5 +1,4 @@
 // netlify/functions/bonkscreener.js
-const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
   const url = 'https://launch-mint-v1.raydium.io/get/list?platformId=FfYek5vEz23cMkWsdJwG2oa6EphsvXSHrGpdALN4g6W1,BuM6KDpWiTcxvrpXywWFiw45R2RNH8WURdvqoTDV1BW4&sort=new&size=100&mintType=default&includeNsfw=false';
